94b6249b5342cf9659ca65676221664db0664ba0
Gabriel Carneiro <bieusc@hotmail.com>
2022-05-30 17:56:13 -0300

    colocando o dark mode e consertando alguns erros visuais

--- projeto/routes/cartoes.js
+++ projeto/routes/cartoes.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('cartoes/cartoes', { title: 'SyncPay - Cartões', style: 'stylesheets/Cartoes.css' });
+router.get("/", function (req, res, next) {
+  res.render("cartoes/cartoes", {
+    title: "SyncPay - Cartões",
+    style: "stylesheets/Cartoes.css",
+  });
 });
 
 module.exports = router;
--- projeto/routes/principal.js
+++ projeto/routes/principal.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("principal/principal", {title: 'SyncPay - Principal', style: 'stylesheets/Principal.css', icons: 'stylesheets/boxicons.min.css'});
+  res.render("principal/principal", {
+    title: "SyncPay - Principal",
+    style: "stylesheets/Principal.css",
+    icons: "stylesheets/boxicons.min.css",
+  });
 });
 
 module.exports = router;
